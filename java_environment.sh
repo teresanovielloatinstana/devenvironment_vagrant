e7e0cdc36c3d43faf6d0ed6be12a5aa23ecdbbfb
Teresa Noviello <85874226+teresanovielloatinstana@users.noreply.github.com>
2021-08-16 13:33:06 +0200

    Improved user management in scripts

--- java_environment.sh
+++ java_environment.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash  
+USER=teresalili
 echo "Java Script, run by: "$(whoami)
 
 # Maven
@@ -10,12 +11,12 @@ echo "Installing JAVA";
 dnf -y install java-1.8.0-openjdk.x86_64 ;
 
 # ANT
-ANT_PATH=/home/teresalili/apache-ant
+ANT_PATH=/home/$USER/apache-ant
 
 if [ ! -d "$ANT_PATH" ]; then
 
 	echo "Installing ANT";
-	cd /home/teresalili ; 
+	cd /home/$USER ; 
 	curl -L -o apache-ant-1.10.11-bin.zip https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.zip ;
 	unzip apache-ant-1.10.11-bin.zip >> /dev/null 2>&1 ; 
 	
@@ -23,7 +24,7 @@ if [ ! -d "$ANT_PATH" ]; then
 		rm -rf apache-ant-1.10.11-bin.zip ;
 		mv apache-ant-1.10.11 apache-ant ;
 		ln -fs apache-ant apache-ant-1.10.11 ;
-		chown --recursive teresalili.teresalili apache-ant apache-ant-1.10.11 ;
+		chown --recursive $USER.$USER apache-ant apache-ant-1.10.11 ;
 	else
 		echo "Apache ANT was not correctly installed: please, rerun the script with debug enabled and check the logs"
 	fi; 
