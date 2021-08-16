a9643f6d713803423b1f83a407922c1190b56892
Teresa Noviello <85874226+teresanovielloatinstana@users.noreply.github.com>
2021-08-16 10:03:55 +0200

    1. Static IP 2. Improved the user envornment 3. Added user custom prompt

--- user_environment.sh
+++ user_environment.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash  
 USER=teresalili
 echo "Add user to docker,wheel group";
- usermod -a -G docker,wheel teresalili
+usermod -a -G docker,wheel teresalili
 # Add user's own aliases and environment variables
 echo "Modify User Environment";
+mv /home/teresalili/.bashrc.d /home/teresalili/bashrc.d.bkp
 cp -r /vagrant/.bashrc.d/ /home/teresalili/;  
+cd /home/teresalili/.bashrc.d/;
+dos2unix ./env;
+dos2unix ./alias;
+cd /home/teresalili;
 chmod --recursive 775 /home/teresalili/.bashrc.d/;
 chown --recursive teresalili.teresalili /home/teresalili/.bashrc.d/;
 
@@ -12,6 +17,7 @@ cp /vagrant/.bashrc /home/teresalili/.bashrc ;
 cd /home/teresalili;  
 dos2unix /home/teresalili/.bashrc ;  
 chown teresalili.teresalili /home/teresalili/.bashrc ; 
+
 cd;
 mkdir -p /home/teresalili/DEV ;  
 mkdir -p /home/teresalili/DEV/git ;
@@ -20,4 +26,5 @@ ln -fs /learning learning ;
 cd ;
 chown --recursive teresalili.teresalili /home/teresalili/DEV ; cd ;
 mkdir -p /home/teresalili/AGENT_INSTALL ;  chown --recursive teresalili.teresalili /home/teresalili/AGENT_INSTALL ; cd ;
+mv /home/teresalili/.config /home/teresalili/config.bkp ;
 cp -r /vagrant/.config /home/teresalili/ ;  chown --recursive teresalili.teresalili /home/teresalili/.config ; cd ;
